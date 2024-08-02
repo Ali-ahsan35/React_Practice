441e86044b7df345469d1e15e930685055a037dc
Ali-ahsan35 <syedahsan748@gmail.com>
2024-08-03 02:15:22 +0600

    Make a  component name chai

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,16 @@
+import Chai from "./chai"
+
 function App() {
   return (
+    <>
     <h1>
-      Chai aur react with vite | S A Ahsan :- This is first react project. In
-      this project I learn how to install react with vite. I also delete thos
-      file which are not important right now. I also learn to how to display
-      Hello world
+    Chai aur react with vite | S A Ahsan :- This is first react project. In
+    this project I learn how to install react with vite. I also delete thos
+    file which are not important right now. I also learn to how to display
+    Hello world
     </h1>
+    <Chai/>
+    </>
   );
 }
 
