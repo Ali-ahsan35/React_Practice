c7cc6f161d853791fc91703e1e978d3ab7b0a322
Ali-ahsan35 <syedahsan748@gmail.com>
2024-08-02 22:14:25 +0600

    Install react with vite and display hello world

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <h1>
+      Chai aur react with vite | S A Ahsan :- This is first react project. In
+      this project I learn how to install react with vite. I also delete thos
+      file which are not important right now. I also learn to how to display
+      Hello world
+    </h1>
+  );
+}
+
+export default App;
