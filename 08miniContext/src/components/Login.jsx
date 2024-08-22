44d0c5cda34e61a336db35b63896ad33ada4e3d0
Ali-ahsan35 <syedahsan748@gmail.com>
2024-08-22 16:31:43 +0600

    some minor chages added

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useContext } from "react";
-import UserContext from "../context/userContext";
+import UserContext from "../context/UserContext";
 
 function Login() {
   const [username, setUsername] = useState("");
